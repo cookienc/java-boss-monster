eb170f0f113dc391d7b38a0d1ed9807f5bfc4e3f
Ethan <kim76744dev@gmail.com>
2023-09-02 19:01:05 +0900

    feat: Boss 공격 기능 추가

--- src/main/java/bossmonster/domain/Boss.java
+++ src/main/java/bossmonster/domain/Boss.java
@@ -1,13 +1,24 @@
 package bossmonster.domain;
 
+import bossmonster.domain.vo.Damage;
 import bossmonster.domain.vo.HealthPoint;
 
-public class Boss {
+public class Boss extends Damageable implements Attackable {
 
     private final HealthPoint healthPoint;
 
     public Boss(final HealthPoint healthPoint) {
         this.healthPoint = healthPoint;
+    }
+
+    @Override
+    public void attack(final Damageable target, final AttackSkill attackSkill) {
+        target.minusHealthPoint(attackSkill.getAttackDamage());
+    }
+
+    @Override
+    protected void minusHealthPoint(final Damage attackDamage) {
+        healthPoint.minus(attackDamage);
     }
 
     public HealthPoint getHealthPoint() {
